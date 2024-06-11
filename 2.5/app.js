0be0741b97997a1d3ab220f657e02732effddcb0
SurajNyavanandi <147505010+SurajNyavanandi@users.noreply.github.com>
2024-06-11 18:21:32 +0530

    Add files via upload

---
+++ 2.5/app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const path=require('path');
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+const bodyParser=require('body-parser');
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+//Routes
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+//ErrorHandling
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
+
+
+
+app.listen(7000,()=>console.log(`Server started running on port 7000......`));
