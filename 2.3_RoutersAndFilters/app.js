7dcced27db1dcdbf4aeb215e951fb86e486e638c
SurajNyavanandi <147505010+SurajNyavanandi@users.noreply.github.com>
2024-06-11 09:45:41 +0530

    Add files via upload

---
+++ 2.3_RoutersAndFilters/app.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+const bodyParser=require('body-parser');
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Routes
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+//ErrorHandling
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>');
+})
+
+
+
+app.listen(7000,()=>console.log(`Server started running on port 7000......`));
