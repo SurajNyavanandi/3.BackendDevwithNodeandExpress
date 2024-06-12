c9221177d0c7972b730c66b80f04d98d36f3c8fc
SurajNyavanandi <147505010+SurajNyavanandi@users.noreply.github.com>
2024-06-12 06:57:10 +0530

    Add files via upload

---
+++ 2.6_Controllers/routes/admin.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const productController=require('../controllers/products')
+
+const router = express.Router();
+
+
+
+// /admin/add-product => GET
+router.get('/add-product',productController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product',productController.postPostAddProduct );
+
+// exports.routes = router;
+// exports.products = products;
+module.exports=router;
