20e3673ba026b44067ecbc9d0a812fbb86cfa6dd
SurajNyavanandi <147505010+SurajNyavanandi@users.noreply.github.com>
2024-07-13 16:05:05 +0530

    Update userController.js

--- ExpenseTracker/controllers/userController.js
+++ ExpenseTracker/controllers/userController.js
@@ -8,3 +8,16 @@ exports.createUserSignUp=async (req,res)=>{
        res.status(500).send({Error:'Creating sign up details'}); 
     }
 }
+exports.createUserLogin = async (req, res) => {
+   try {
+       const { email, password } = req.body;
+       const user = await SignUp.findOne({ where: { email: email } });
+       
+       if (!user || user.password !== password) {
+           return res.status(400).json({ Error: 'Invalid email or password' });
+       }
+       res.status(200).json({ Message: 'User login successful' });
+   } catch (error) {
+       res.status(500).send({ Error: 'Error checking login details' });
+   }
+};
